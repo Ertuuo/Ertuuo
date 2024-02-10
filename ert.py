4d139db870d9e372fd37eff32a98a367eb6fc067
Ertuuo <157130537+Ertuuo@users.noreply.github.com>
2024-02-10 13:29:34 +0300

    Update ert.py
    süü

--- ert.py
+++ ert.py
@@ -1,14 +1,28 @@
-meme_dict = {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            "ROFL": "Bir şakaya karşılık cevap",
-            "SHEESH": "Onaylamak",
-            "CREEPY": "Korkunç",
-            "AGGRO": "Agresifleşmek/Sinirlenmek"}
-            
-while True:            
-    word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")   
-    if word in meme_dict.keys():
-        print(meme_dict[word])
-    else:
-        print("Hiç bir fikrim yok")
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command(description='Çünkü skoru başka bir şekilde halletmek istediğinde')
+async def banasecimyapbunlarla(ctx, *choices: str):
+    await ctx.send(random.choice(choices))
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("MTIwMzI3ODk4OTUyMzgyMDU1NA.GYVRKf.NHVxm9ajwkyydWV8rtA5ftDGKZtB8x2lwwRFY0")
