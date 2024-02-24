79963c91b9e8c1e8a9111e89b7f9a19393f2b9fc
Ertuuo <157130537+Ertuuo@users.noreply.github.com>
2024-02-24 13:30:33 +0300

    Update ert.py

--- ert.py
+++ ert.py
@@ -1,28 +1,45 @@
 import discord
 from discord.ext import commands
+import time
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
 
-@bot.command(description='Çünkü skoru başka bir şekilde halletmek istediğinde')
-async def banasecimyapbunlarla(ctx, *choices: str):
-    await ctx.send(random.choice(choices))
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+async def slm(ctx):
+    await ctx.send(f'Selam {bot.user}! Ben bir çevreci botum!')
+
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def ne_yaparsin (ctx):
+    await ctx.send(f"Soru şiklari; bir, ne yapababilirim. iki, video önerisi. üç, çevre koruma önlemleri. Bu şıkları lütfen yazı ile yaz  ")
 
-bot.run("MTIwMzI3ODk4OTUyMzgyMDU1NA.GYVRKf.NHVxm9ajwkyydWV8rtA5ftDGKZtB8x2lwwRFY0")
+
+@bot.command()
+async def bir (ctx):
+    await ctx.send(f"Senin sorularını cevaplarım bu kadar :-D")
+
+@bot.command()
+async def iki (ctx):
+    await ctx.send(f"Adım 1) Google Amca'ya (ve ya teyzeye) YouTube yaz.")
+    time.sleep(1)
+    await ctx.send(f"Adım 2) Çıkan ilk sekmeye tıkla.")
+    time.sleep(1)
+    await ctx.send(f"Adım 3) Orada çıkan arama sekmesine istediğin şeyi yaz ve işlem bitti. :-P ")
+
+@bot.command()
+async def üç (ctx):
+    await ctx.send(f"Çöplerini ayrıştır bu tüm dünyaya yeter.")
+    time.sleep(1)
+    await ctx.send(f"Eğer maddi durumun varsa elektrikli araba al.(Mesela Togg.)")
+    time.sleep(1)
+    await ctx.send(f"Bunun gibi pek çok örnek olabilir.")
+
+bot.run("MTIwODM1NTk4OTgxMjk0NDkxNw.Gal2rF.f0m5a5yVPTx1fNTfes26QCiePUzjZjhC0Mrmgc")
